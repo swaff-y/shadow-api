5bc599cac74cf5e3ee962420d1653de079d881be
swaff-y <kyle@swaff.id.au>
2026-03-26 21:38:50 +1100

    Add company enum

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,8 @@
 class Company < ApplicationRecord
   has_many :users
 
+  enum :status, { active: "active", trial: "trial", suspended: "suspended", cancelled: "cancelled" }
+
   validates :name, presence: true
+  validates :status, presence: true
 end
